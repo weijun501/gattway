3cfead96f7d151a153584a3151bc2f36cf3ce125
weijun.zhang <weijun.zhang@outfit7.com>
2022-10-12 15:28:31 +0800

    first commit

---
+++ dynamicfeature/src/main/java/com/zwj/dynamicfeature/App1.kt
@@ -0,0 +1,14 @@
+package com.zwj.dynamicfeature
+
+import android.content.Context
+
+/**
+ * @author zhangweijun
+ * @date 2022/10/9 18:43
+ * @description:
+ */
+class App1 constructor(context: Context){
+
+
+
+}
